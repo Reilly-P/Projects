34397d2b25c4876fe55051f7a8f3e8ae1d508186
Reilly Prescott <rjsgoodeggs@gmail.com>
2022-09-05 17:23:48 -0600

    Hour Conversion Final

--- Hour Conversion.py
+++ Hour Conversion.py
@@ -1,14 +1,15 @@
 TimeHours = input("What is the current time in hours: ")
 TimeWait = input("How long do I wait: ")
-AM = 0
-PM = 1
-AMPM = input ("AM(0) or PM(1): ")
+#Here is a little bit I added to make sure I could track AM and PM on the clock. Still new so I had it ask for 0 or 1 to determine AM/PM.
+AMPM = input ("AM (0) or PM (1): ")
 AMPM = int(AMPM)
+#This next bit is to check if someone puts in a number bigger than 12. If they do, it reduces it until it is less than 12 and changes the AM/PM variable accordingly.
 TimeHours = int(TimeHours)
 if TimeHours > 12:
     TimeHours = TimeHours-12
     AMPM = AMPM + 1
 TimeHours = str(TimeHours)
+#This part just confirms the current time.
 if AMPM % 2 == 0:
     print("The current time is: " + TimeHours + "AM")
 else:
@@ -20,6 +21,7 @@ while TimeWait > 12:
 TimeWait = str(TimeWait)
 TimeTotal = int(TimeWait) + int(TimeHours)
 TimeTotal = str(TimeTotal)
+#This last part shows what time it will be.
 if AMPM % 2 == 0:
     print ("The time will be: " + TimeTotal + "AM")
 else:
