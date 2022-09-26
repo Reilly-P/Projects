b1ff1c3854573e7af9885b3a81c166a8f163444a
Reilly Prescott <rjsgoodeggs@gmail.com>
2022-09-26 16:52:15 -0600

    Functions

---
+++ Conditional/Conditional.py
@@ -0,0 +1,13 @@
+def Sum():
+    print(a+b+c+d+e)
+def Product():
+    print(a*b*c*d*e)
+def Avg():
+    print((a+b+c+d+e)/5)
+def Largest():
+    Five = [a, b, c, d, e]
+    for i in Five:
+        if i >= a & b & c & d & e:
+            print(i, "Is the largest number of the set")
+        else:
+            return
