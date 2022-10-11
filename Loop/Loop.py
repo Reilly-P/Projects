949064c5509fd33c6eb5811c227a7fc29bce0e83
Reilly Prescott <rjsgoodeggs@gmail.com>
2022-10-10 18:31:42 -0600

    loop

---
+++ Loop/Loop.py
@@ -0,0 +1,5 @@
+#My name is Reilly Prescott and this is my Number Guess program
+import random
+number = random.randrange(1,20)
+name = input("What's your name? ", )
+print("Hello! ", name, "welcome to my number guess game!")
