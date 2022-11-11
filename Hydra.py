a60c24c946769243e3f00bbb5103cc4e22def7c0
Reilly-P <112213409+Reilly-P@users.noreply.github.com>
2022-11-10 23:21:45 -0700

    Hydra

---
+++ Hydra.py
@@ -0,0 +1,45 @@
+H = 0
+T = 0
+S = 0
+def Hydra():
+    global T
+    global H
+    global S
+    if H == 0 and T == 0:
+        return
+    if H % 2 != 0:
+        T -= 2
+        H += 1
+        #print(H,T)
+        S += 1
+    if T % 4 != 0:
+        while True:
+            T += 1
+            #print(H,T)
+            S += 1
+            if T % 4 == 0:
+                break
+    if H % 2 == 0:
+        H -= 2
+        #print(H,T)
+        S += 1
+    if T % 4 == 0:
+        while True:
+            T -= 2
+            H += 1
+            #print(H,T)
+            S += 1
+            if T == 0:
+                break
+    if H >= T:
+        while True:
+            H -= 2
+            #print(H,T)
+            S += 1
+            if H <= T:
+                break
+        
+while H != 0 and T != 0:       
+    Hydra()
+if H == 0 and T == 0:
+    print(S)
